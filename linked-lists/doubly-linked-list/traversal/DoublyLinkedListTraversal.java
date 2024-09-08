42dc69cfc08b3e93f13370b9837ba67efb3640f2
Prashant <srprashant4@gmail.com>
2024-09-08 16:58:19 +0530

    Added the code for doubly linked lists traversal techniques

---
+++ linked-lists/doubly-linked-list/traversal/DoublyLinkedListTraversal.java
@@ -0,0 +1,57 @@
+/**
+ * This class demonstrates the forward and backward traversal techniques in a doubly linked list.
+ * 
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.next = prev;
+    }
+}
+
+public class DoublyLinkedListTraversal {
+
+    public static void forwardTraversal(Node head) {
+        Node curr = head;
+
+        while(curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public static void backwardsTraversal(Node tail) {
+        Node curr = tail;
+
+        while(curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.prev;
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+
+        head.next = second;
+        second.prev = head;
+        second.next = third;
+        third.prev = second;
+
+        System.out.println("Forward traversal in doubly linked list: ");
+        forwardTraversal(head);
+
+        System.out.println("Backwards traversal in doubly linked list: ");
+        backwardsTraversal(third);
+    }
+}

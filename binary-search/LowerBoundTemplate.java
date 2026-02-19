cd3a8894363f50f670e9e69e74114f0a9bf44f95
Prashant <srprashant4@gmail.com>
2026-02-20 00:05:02 +0530

    Minor Changes in the LowerBoundTemplate.

--- binary-search/LowerBoundTemplate.java
+++ binary-search/LowerBoundTemplate.java
@@ -12,6 +12,14 @@
  * 
  * - We are using a half-open interval [left, right) to maintain the search space.
  * - The loop continues until left == right, at which point left is the insertion position for the target.
+ * - What this function returns depends on the presence and position of the target in the array:
+ * | Scenario                | Return Value     |
+ * | ----------------------- | ---------------- |
+ * | Target exists           | First occurrence |
+ * | Target missing          | Insertion index  |
+ * | Target smaller than all | 0                |
+ * | Target larger than all  | n                |
+ * 
  * 
  * Example:
  * Input: arr = [1, 2, 3, 4, 5, 6, 7], target = 6
